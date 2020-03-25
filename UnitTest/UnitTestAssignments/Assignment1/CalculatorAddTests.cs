653fddf8aa13e680a6eb8df3001d8c03533b72a1
Brian <brian@symax.nl>
2020-03-25 10:48:00 +0100

    Add more calculatorAdd tests

--- UnitTest/UnitTestAssignments/Assignment1/CalculatorAddTests.cs
+++ UnitTest/UnitTestAssignments/Assignment1/CalculatorAddTests.cs
@@ -21,5 +21,57 @@ namespace UnitTest.UnitTestAssignments.Assignment1
             //Assert
             Assert.AreEqual(7, result);
         }
+
+        [TestMethod]
+        public void WhenAddingTheNumberTwelveAndSixTheResultShouldBeEighteen()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.Add(12, 6);
+
+            //Assert
+            Assert.AreEqual(18, result);
+        }
+
+        [TestMethod]
+        public void WhenAddingTheNumberThreeAndMinus9TheResultShouldBeMinusSix()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.Add(3, -9);
+
+            //Assert
+            Assert.AreEqual(-6, result);
+        }
+
+        [TestMethod]
+        public void WhenAddingTheNumberMinusTenAnElevenTheResultShouldBeOne()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.Add(-10, 11);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WhenAddingTheNumberMinusFiveAnMinusFourTheResultShouldBeOne()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.Add(-5, -4);
+
+            //Assert
+            Assert.AreEqual(-9, result);
+        }
     }
 }
